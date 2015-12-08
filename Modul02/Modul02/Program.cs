308726ddc664e5e49dc7a474f08badb6e08246b9
ibotev <ibotev@gbg.bg>
2015-12-08 22:48:59 +0200

    Modul 02, Video 04 - Rabota sas stringove

--- Modul02/Modul02/Program.cs
+++ Modul02/Modul02/Program.cs
@@ -10,11 +10,18 @@ namespace Modul02
 			//Дефиниране на променливи
 			int a = 0, b = 0;
 			int sum = 0;
+			string program_name = "Modul02 - Primeri";
+			string program_version = "1.0";
+
 
 			//Събиране на числа
 			a = 10;
 			b = 5;
 			sum = a + b;
+
+			//Za programata
+			Console.WriteLine ( program_name );
+			Console.WriteLine ( "Version: " + program_version + "\n\n");
 
 			//Писане в конзола
 			Console.Write (a);
@@ -35,9 +42,13 @@ namespace Modul02
 			Console.WriteLine ("\nIzpolzwane na *=");
 			a *= b;
 			Console.WriteLine (a);
+			a = 15; b = 10;
+			Console.WriteLine ("\n Standartno delene: " + ( 14/4 ). ToString());
+			Console.WriteLine ("\n Hvastane na ostatak: " + (14 % 4).ToString ());
 
-			Console.WriteLine ("\nStandartno delene: " + ( 14/4 ). ToString());
-			Console.WriteLine ("standartno delene: " + (14 % 4).ToString ());
+			Console.WriteLine (a % b);		//pokazwa ostataka
+
+			Console.WriteLine ( program_name );
 		}
 	}
 }
