f436c5273c4e7cd90af19d9874a3f2f839ef793e
ibotev <ibotev@gbg.bg>
2015-12-08 22:21:54 +0200

    Модул 02, Видео 03 - Основни оператори за целочислен тип данни

--- Modul02/Modul02/Program.cs
+++ Modul02/Modul02/Program.cs
@@ -12,17 +12,32 @@ namespace Modul02
 			int sum = 0;
 
 			//Събиране на числа
-			a = 7;
-			b = 6;
+			a = 10;
+			b = 5;
 			sum = a + b;
 
 			//Писане в конзола
 			Console.Write (a);
-			Console.Write (" плюс ");
+			Console.Write (" plus ");
 			Console.Write (b);
-			Console.Write ( " равно на ");
+			Console.Write ( " ravno na ");
 			Console.WriteLine (sum);
 
+			//Допълнителни оператори
+			Console.WriteLine ("\nIzpolzwane na +=");
+			a += b; //a=12+6, a=a+b
+			Console.WriteLine (a);
+
+			Console.WriteLine ("\nIzpolzwane na -=");
+			a -= b;
+			Console.WriteLine (a);
+
+			Console.WriteLine ("\nIzpolzwane na *=");
+			a *= b;
+			Console.WriteLine (a);
+
+			Console.WriteLine ("\nStandartno delene: " + ( 14/4 ). ToString());
+			Console.WriteLine ("standartno delene: " + (14 % 4).ToString ());
 		}
 	}
 }
